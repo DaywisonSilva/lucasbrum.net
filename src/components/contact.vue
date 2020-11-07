205eb000626a160d87cec3e3594026fe9d4f19fd
Lucas Saliés Brum <lucas@archlinux.com.br>
2020-11-07 00:40:01 -0400

    Autocommit por GitHub: alterações: add 'src/router/index.js' add 'src/components/contact.vue'

---
+++ src/components/contact.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <h1>Contact</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'contact',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
