703c018d89a74c62e09fb6cd1afd1f432cd3e60d
Lucas Saliés Brum <lucas@archlinux.com.br>
2020-11-07 01:15:01 -0400

    Autocommit por GitHub: alterações: add 'src/App.vue' remove 'static/css/main.css' add 'static/main.css'

--- src/App.vue
+++ src/App.vue
@@ -17,6 +17,9 @@ export default {
 </script>
 
 <style>
+@import 'https://fonts.googleapis.com/css2?family=Indie+Flower&family=Open+Sans&display=swap';
+@import '@assets/css/main.css';
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
