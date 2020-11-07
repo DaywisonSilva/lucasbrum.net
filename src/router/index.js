577f1f67e2c9c430fc8af15cc5cdcb1d00745b0e
Lucas Saliés Brum <lucas@archlinux.com.br>
2020-11-07 00:34:01 -0400

    Autocommit por GitHub: alterações: add 'src/router/index.js'

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,12 @@
+// Main
 import Vue from 'vue'
 import Router from 'vue-router'
+
+// Seções
 import home from '@/components/home'
+import blog from '@/components/blog'
+import services from '@/components/services'
+import contact from '@/components/contact'
 
 Vue.use(Router)
 
