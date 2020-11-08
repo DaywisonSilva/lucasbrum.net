a5aa4c283d8ca81138cdb2210d99513c29184ae2
Lucas Saliés Brum <lucas@archlinux.com.br>
2020-11-08 19:04:01 -0400

    Autocommit por GitHub: alterações: add 'src/App.vue'

--- src/App.vue
+++ src/App.vue
@@ -34,6 +34,8 @@ export default {
       //alert('Olá ' + this.name + '!')
       // `event` é o evento DOM nativo
 
+      dark = dark ? false : true
+
       // darkMode
       if (event) {
         alert(event.target.tagName)
