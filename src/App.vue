299ba118e6d4054a175d77e6e62ebbdcbf75ef96
Lucas Saliés Brum <lucas@archlinux.com.br>
2020-11-07 20:58:34 -0400

    Update

--- src/App.vue
+++ src/App.vue
@@ -15,6 +15,7 @@
     </div>
   </div>
 </template>
+
 <script>
 export default {
   name: 'App',
@@ -29,26 +30,11 @@ export default {
 <style>
 @import 'https://fonts.googleapis.com/css2?family=Indie+Flower&family=Open+Sans&display=swap';
 @import './assets/css/main.css';
+
 #app {
   font-family: 'Open Sans', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
-  margin-top: 60px;
-}
-
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
 }
 </style>
