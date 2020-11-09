210a7a59e3a4a15c914fffcc371a2c6b43dddc98
Lucas Saliés Brum <lucas@archlinux.com.br>
2020-11-09 19:13:01 -0400

    Autocommit por GitHub: alterações: add 'src/components/projetos.vue'

--- src/components/projetos.vue
+++ src/components/projetos.vue
@@ -1,21 +1,35 @@
 <template>
   <div class="home">
-    <h1>{{ titulo }}<span>{{ subtitulo }}</span></h1>
+    <h1>
+      {{ titulo }}<span>{{ subtitulo }}</span>
+    </h1>
     <p>Em breve...</p>
   </div>
 </template>
 <script>
 export default {
-  name: 'projetos',
-  data () {
+  name: "projetos",
+  data() {
     return {
-      subtitulo: 'Projetos'
-    }
+      subtitulo: "Projetos",
+    };
   },
   props: {
-    titulo: String
-  }
-}
+    titulo: String,
+  },
+  mounted() {
+    let url = "https://api.github.com/users/sistematico/repos";
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        for (const nome of data.name) {
+          console.log(name);
+
+        }
+      })
+      .catch(console.error);
+  },
+};
 </script>
 <style scoped>
 </style>
