f76e2648f14a613e14b0dae8b46d584de0050436
Lucas Saliés Brum <lucas@archlinux.com.br>
2020-11-09 11:03:01 -0400

    Autocommit por GitHub: alterações: add 'src/App.vue'

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,24 @@
   <div id="app">
     <div class="container">
       <aside>
-        <img class="logo" src="./assets/logo.png" alt="Lucas Saliés Brum">
-      </aside>  
+        <img class="logo" src="./assets/logo.png" alt="Lucas Saliés Brum" />
+      </aside>
       <main>
-        <div class="lights"><i :class="[dark ? 'fas fa-moon' : 'fas fa-sun']" @click="switchTheme"></i></div>
+        <div class="lights">
+          <i
+            :class="[dark ? 'fas fa-moon' : 'fas fa-sun']"
+            @click="switchTheme"
+          ></i>
+        </div>
         <router-view :subtitulo="subtitulo" />
         <ul>
           <li><router-link class="nav-link" to="/">Início</router-link></li>
-          <li><router-link class="nav-link" to="/projetos">Projetos</router-link></li>
-          <li><router-link class="nav-link" to="/contato">Contato</router-link></li>
+          <li>
+            <router-link class="nav-link" to="/projetos">Projetos</router-link>
+          </li>
+          <li>
+            <router-link class="nav-link" to="/contato">Contato</router-link>
+          </li>
         </ul>
       </main>
     </div>
@@ -18,16 +27,16 @@
 </template>
 
 <script>
-import { darkMode } from '@/assets/js/darkmode'
+import { darkMode } from "@/assets/js/darkmode";
 
 export default {
-  name: 'App',
-  data: function() { 
+  name: "App",
+  data: function () {
     return {
-      subtitulo: 'Desenvolvedor PHP & JavaScript',
-      dataTheme: 'dark',
-      dark: false
-    }
+      subtitulo: "Desenvolvedor PHP & JavaScript",
+      dataTheme: "dark",
+      dark: false,
+    };
   },
   methods: {
     switchTheme: function (event) {
@@ -35,22 +44,52 @@ export default {
       //alert('Olá ' + this.name + '!')
       // `event` é o evento DOM nativo
 
-      this.dark = this.dark ? false : true
+      this.dark = this.dark ? false : true;
 
       // darkMode
       //if (event) {
       //  alert(event.target.tagName)
       //}
-    }
-  }
-}
+    },
+    mounted() {
+      // set page title
+      document.title = "Lucas Saliés Brum - WebDev";
+      // set 'app-background' class to body tag
+      let bodyElement = document.body;
+      bodyElement.classList.add("app-background");
+      // check for active theme
+      let htmlElement = document.documentElement;
+      let theme = localStorage.getItem("theme");
+      if (theme === "dark") {
+        htmlElement.setAttribute("theme", "dark");
+        this.darkMode = true;
+      } else {
+        htmlElement.setAttribute("theme", "light");
+        this.darkMode = false;
+      }
+    },
+    watch: {
+      darkMode: function () {
+        // add/remove class to/from html tag
+        let htmlElement = document.documentElement;
+        if (this.darkMode) {
+          localStorage.setItem("theme", "dark");
+          htmlElement.setAttribute("theme", "dark");
+        } else {
+          localStorage.setItem("theme", "light");
+          htmlElement.setAttribute("theme", "light");
+        }
+      },
+    },
+  },
+};
 </script>
 
 <style>
-@import '../static/css/darkmode.css';
+@import "../static/css/darkmode.css";
 
 #app {
-  font-family: 'Open Sans', Helvetica, Arial, sans-serif;
+  font-family: "Open Sans", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
