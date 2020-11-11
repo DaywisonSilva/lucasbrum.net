45c35a356d2752a45a2a478126cf1f051066191e
Lucas Saliés Brum <lucas@archlinux.com.br>
2020-11-10 20:29:01 -0400

    Autocommit por GitHub: alterações: add 'src/components/projetos.vue'

--- src/components/projetos.vue
+++ src/components/projetos.vue
@@ -59,12 +59,13 @@ export default {
 </script>
 <style scoped>
 .loader {
-  border: 16px solid #f3f3f3; /* Light grey */
-  border-top: 16px solid #3498db; /* Blue */
+  border: 8px solid #f3f3f3; /* Light grey */
+  border-top: 8px solid #3498db; /* Blue */
   border-radius: 50%;
-  width: 60px;
-  height: 60px;
+  width: 30px;
+  height: 30px;
   animation: spin 1s linear infinite;
+  text-align: center;
 }
 
 @keyframes spin {
